6e90211e2cb19bcba251900b0fc6656cc0ae2411
Yuki Janvier <yuki@tamu.edu>
2023-10-19 18:56:28 +0000

    finished this file

--- types.c
+++ types.c
@@ -3,7 +3,36 @@
 #include <string.h>
 #include <sys/stat.h>
 
-int main(int argc, char *argv[])
-{
-    
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr,"usage: path1 [path2 ...]\n");
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        char *file_path = argv[i];
+        struct stat file_info;
+
+        if (stat(file_path, &file_info) == 0) {
+            if (S_ISREG(file_info.st_mode)) {
+                printf("%s: regular file\n", file_path);
+            } else if (S_ISDIR(file_info.st_mode)) {
+                printf("%s: directory\n", file_path);
+            } else if (S_ISLNK(file_info.st_mode)) {
+                printf("%s: symbolic link\n", file_path);
+            } else if (S_ISCHR(file_info.st_mode)) {
+                printf("%s: character special file\n", file_path);
+            } else if (S_ISBLK(file_info.st_mode)) {
+                printf("%s: block special file\n", file_path);
+            } else if (S_ISFIFO(file_info.st_mode)) {
+                printf("%s: FIFO/pipe\n", file_path);
+            } else if (S_ISSOCK(file_info.st_mode)) {
+                printf("%s: socket\n", file_path);
+            }
+        } else {
+            fprintf(stderr, "%s: path error\n", file_path);
+        }
+    }
+
+    return 0;
 }
